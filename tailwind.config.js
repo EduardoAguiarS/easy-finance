3f71968d22c8e1ae7b812d18469a09dd88d3f532
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-01-16 23:33:18 -0300

    start project next/tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      orange: "#F04E23",
+      yellow: "#FFB400",
+      white: "#FFF8F1",
+      blueDark: "#04294F",
+      blue: "#5B66CD",
+      blueLight: "#00AEE0"
+    }
+  },
+  plugins: []
+};
